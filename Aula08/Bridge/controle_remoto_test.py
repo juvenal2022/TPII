3b39149f9c8462c74854d77f6f530cd9c1a96e14
jubabdag <juvenal.yoshikawa@fatec.sp.gov.br>
2026-03-31 13:44:40 -0300

    Adicionando pasta Aula08

---
+++ Aula08/Bridge/controle_remoto_test.py
@@ -0,0 +1,28 @@
+from controle_remoto import TV, Radio, ControleRemoto, ControleAvancado
+
+def test_tv_ligar(capsys):
+    tv = TV()
+    controle = ControleRemoto(tv)
+
+    controle.ligar()
+
+    captured = capsys.readouterr()
+    assert "TV ligada" in captured.out
+
+def test_radio_volume(capsys):
+    radio = Radio()
+    controle = ControleAvancado(radio)
+    
+    controle.aumentar_volume()
+
+    captured = capsys.readouterr()
+    assert "Volume do Rádio: 20" in captured.out
+
+def test_tv_desligar(capsys):
+    tv = TV()
+    controle = ControleRemoto(tv)
+
+    controle.desligar()
+
+    captured = capsys.readouterr()
+    assert "TV desligada" in captured.out
